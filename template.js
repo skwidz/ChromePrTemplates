9277fb9b940960ed32eec6858a3d6291a6ca6045
skwidz <christian.e@getjobber.com>
2020-01-27 12:38:38 -0700

    make template function look nicer

--- template.js
+++ template.js
@@ -1,3 +1,6 @@
-chrome.storage.sync.get('templateText', function(data) {
-  document.getElementById("pull_request_body").value = data.templateText
-})
+function set_template(){
+  chrome.storage.sync.get('templateText', function(data) {
+    document.getElementById("pull_request_body").value = data.templateText
+  })
+};
+set_template();
